b4efa47e6588b50d1b39c4a6e90df271a33e39da
Sergey Ivanov <voivoid@mail.ru>
2018-01-18 00:25:45 +0300

    problem2a

--- adventofcode.erl
+++ adventofcode.erl
@@ -1,13 +1,23 @@
 -module(adventofcode).
 -compile(export_all).
 
-problem1a(Xs) ->
-    Nums = lists:map(fun (X) -> list_to_integer([X]) end, Xs),
-    F = fun(A, B) -> if A == B -> A; A /= B -> 0 end end,
-    lists:sum( lists:zipwith( F, Nums, tl(Nums) ++ [hd(Nums)] ) ).
+% https://adventofcode.com problems
 
-problem1b(Xs) ->
-    Nums = lists:map(fun (X) -> list_to_integer([X]) end, Xs),
-    {L1, L2} = lists:split(length(Nums) div  2, Nums),
+problem1a(Input) ->
+    Ints = lists:map(fun (X) -> list_to_integer([X]) end, Input),
     F = fun(A, B) -> if A == B -> A; A /= B -> 0 end end,
-    lists:sum( lists:zipwith( F, Nums, L2 ++ L1 ) ).
+    lists:sum( lists:zipwith( F, Ints, tl(Ints) ++ [hd(Ints)] ) ).
+
+problem1b(Input) ->
+    Ints = lists:map(fun (X) -> list_to_integer([X]) end, Input),
+    {L1, L2} = lists:split(length(Ints) div  2, Ints),
+    F = fun(A, B) -> if A == B -> A; A /= B -> 0 end end,
+    lists:sum( lists:zipwith( F, Ints, L2 ++ L1 ) ).
+
+problem2a(Input) ->
+    CalcRowDiff = fun(Row) ->
+       Ints = lists:map(fun list_to_integer/1, string:tokens(Row, " ")),
+       {Min, Max} = {lists:min(Ints), lists:max(Ints)},
+       Max - Min end,
+    RowsDiff = lists:map(CalcRowDiff, string:tokens(Input, "\n")),
+    lists:sum(RowsDiff).
