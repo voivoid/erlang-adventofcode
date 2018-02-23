d936041de7cb6cdebc8d0bea6d6c7e13ad5ca25d
Sergey Ivanov <voivoid@mail.ru>
2018-02-23 20:37:58 +0300

    problem 7b 2017 solved

--- 2017/problem7.erl
+++ 2017/problem7.erl
@@ -1,54 +1,81 @@
 -module(problem7).
--export([solve1/1, solve2/1, findMismatch/1]).
+-export([solve1/1, solve2/1]).
 
 parseTower( Line ) ->
     [ Name, Weight ] = string:tokens( Line, " ()" ),
     { Name, Weight }.
 
-parseTowerWithChildren( [ Tower ] ) ->
-    parseTowerWithChildren( [ Tower, "" ] );
-parseTowerWithChildren( [ Parent, ChildrenStr ] ) ->
+parseTowerAndItsChildren( [ Tower ] ) ->
+    parseTowerAndItsChildren( [ Tower, "" ] );
+parseTowerAndItsChildren( [ Parent, ChildrenStr ] ) ->
     { Name, Weight } = parseTower( Parent ),
     Children = string:tokens( ChildrenStr, " ," ),
     { Name, { Weight, Children } }.
 
 parseInputLine( Line ) ->
-    parseTowerWithChildren( string:tokens( Line, "->" ) ).
+    parseTowerAndItsChildren( string:tokens( Line, "->" ) ).
 
 makeTowerMaps( Input ) ->
     TowerStrs = string:tokens( Input, "\n" ),
     lists:foldl( 
-      fun ( Line, { PtWC, CtP } ) ->
+      fun ( Line, { PtC, CtP, Weights } ) ->
               { Parent, { Weight, Children } } = parseInputLine( Line ),
-              NewPtWC = PtWC#{ Parent => { Weight, Children } },
-              NewCtp = lists:foldl( fun( Child, ChildrenAcc ) -> ChildrenAcc#{ Child => Parent }
+              NewPtC = PtC#{ Parent => Children },
+              NewCtP = lists:foldl( fun( Child, ChildrenAcc ) -> ChildrenAcc#{ Child => Parent }
                                     end,
                                     CtP,
                                     Children ),
-              { NewPtWC , NewCtp } end,
-      { #{}, #{} },
+              NewWeights = Weights#{ Parent => erlang:list_to_integer( Weight ) },
+              { NewPtC, NewCtP, NewWeights } end,
+      { #{}, #{}, #{} },
       TowerStrs ).
 
-findTopParent( ChildToParentMap, Child ) ->
+findTopParentImpl( Child, ChildToParentMap ) ->
     case maps:get( Child, ChildToParentMap, nokey ) of
         nokey -> Child;
-        Parent -> findTopParent( ChildToParentMap, Parent )
+        Parent -> findTopParentImpl( Parent, ChildToParentMap )
     end.
 
-findTopParent( ChildToParentMap ) ->
-    FirstTower = hd( maps:keys( ChildToParentMap ) ),
-    findTopParent( ChildToParentMap, FirstTower ).
-
-findMismatch( [Y,Y,Y|T] ) -> findMismatch( [Y,Y|T] );
-findMismatch( [X,Y,Y|_] ) -> X;
-findMismatch( [Y,X,Y|_] ) -> X;
-findMismatch( [Y,Y,X|_] ) -> X.
-
+findTopParent( ParentToChildrenMap, ChildToParentMap ) ->
+    findTopParentImpl( erlang:hd( maps:keys( ParentToChildrenMap ) ), ChildToParentMap ).
 
 solve1( Input ) ->
-    { _, ChildToParentMap } = makeTowerMaps ( Input ),
-    findTopParent( ChildToParentMap ).
+    { ParentToChildrenMap, ChildToParentMap, _ } = makeTowerMaps( Input ),
+    findTopParent( ParentToChildrenMap, ChildToParentMap ).
+
+
+
+
+
+findMismatch( [{_,Y},{_,Y},{_,Y}|T] ) -> findMismatch( [Y,Y|T] );
+findMismatch( [{E,X},{_,Y},{_,Y}|_] ) -> E - ( X - Y );
+findMismatch( [{_,Y},{E,X},{_,Y}|_] ) -> E - ( X - Y );
+findMismatch( [{_,Y},{_,Y},{E,X}|_] ) -> E - ( X - Y );
+findMismatch( _ ) -> none.
+
+
+getChildren(Parent, ParentToChildrenMap) ->
+    maps:get( Parent, ParentToChildrenMap ).
+
+getWeight(Tower, Weights) ->
+    maps:get( Tower, Weights ).
+
+findWeightMismatch( Parent, ParentToChildrenMap, Weights ) ->
+    ParentWeight = getWeight( Parent, Weights ),
+    case getChildren( Parent, ParentToChildrenMap ) of
+        [] -> { ParentWeight, 0 };
+        Children -> 
+            ChildrenWeights = lists:map( fun( Child ) ->
+                                                 { ChildWeight, SubWeight } = findWeightMismatch( Child, ParentToChildrenMap, Weights ),
+                                                 { ChildWeight, ChildWeight + SubWeight } end,
+                                         Children ),
+            case findMismatch( ChildrenWeights ) of
+                none -> { ParentWeight, lists:sum( [ TotalWeight  || { _, TotalWeight } <- ChildrenWeights ] ) };
+                Correct -> throw(Correct)
+            end
+    end.
 
 solve2( Input ) ->
-    { ParentToChildrenMap, ChildToParentMap } = makeTowerMaps ( Input ),
-    0.
+    { ParentToChildrenMap, ChildToParentMap, Weights } = makeTowerMaps ( Input ),
+    Top = findTopParent( ParentToChildrenMap, ChildToParentMap ),
+    catch findWeightMismatch( Top, ParentToChildrenMap, Weights ).
