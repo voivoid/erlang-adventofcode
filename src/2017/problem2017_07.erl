8e9a0e6c79b274e21e9c6234429698ce00c239dd
Sergey Ivanov <voivoid@mail.ru>
2018-02-25 23:36:15 +0300

    refactor 2017_07

--- src/2017/problem2017_07.erl
+++ src/2017/problem2017_07.erl
@@ -26,41 +26,39 @@ make_tower_maps( Input ) ->
     lists:foldl( 
       fun ( Line, { PtC, CtP, Weights } ) ->
               { Parent, { Weight, Children } } = parse_input_line( Line ),
-              NewPtC = PtC#{ Parent => Children },
-              NewCtP = lists:foldl( fun( Child, ChildrenAcc ) -> ChildrenAcc#{ Child => Parent }
-                                    end,
-                                    CtP,
-                                    Children ),
-              NewWeights = Weights#{ Parent => Weight },
-              { NewPtC, NewCtP, NewWeights } end,
+
+              {
+                PtC#{ Parent => Children },
+                lists:foldl( fun( Child, ChildrenAcc ) -> ChildrenAcc#{ Child => Parent } end, CtP, Children ),
+                Weights#{ Parent => Weight } 
+              }
+      end,
       { #{}, #{}, #{} },
       TowerStrs ).
 
--spec find_top_parent_impl( tower_name(), child_to_parent_map() ) -> tower_name().
-find_top_parent_impl( Child, ChildToParentMap ) ->
+-spec find_base_tower_impl( tower_name(), child_to_parent_map() ) -> tower_name().
+find_base_tower_impl( Child, ChildToParentMap ) ->
     case maps:get( Child, ChildToParentMap, nokey ) of
         nokey -> Child;
-        Parent -> find_top_parent_impl( Parent, ChildToParentMap )
+        Parent -> find_base_tower_impl( Parent, ChildToParentMap )
     end.
 
--spec find_top_parent( parent_to_children_map(), child_to_parent_map() ) -> tower_name().
-find_top_parent( ParentToChildrenMap, ChildToParentMap ) ->
-    find_top_parent_impl( erlang:hd( maps:keys( ParentToChildrenMap ) ), ChildToParentMap ).
+-spec find_base_tower( parent_to_children_map(), child_to_parent_map() ) -> tower_name().
+find_base_tower( ParentToChildrenMap, ChildToParentMap ) ->
+    find_base_tower_impl( erlang:hd( maps:keys( ParentToChildrenMap ) ), ChildToParentMap ).
 
 -spec solve1( nonempty_string() ) -> nonempty_string().
 solve1( Input ) ->
     { ParentToChildrenMap, ChildToParentMap, _ } = make_tower_maps( Input ),
-    find_top_parent( ParentToChildrenMap, ChildToParentMap ).
-
-
+    find_base_tower( ParentToChildrenMap, ChildToParentMap ).
 
 
 
 -spec find_mismatch( [ { integer(), integer() } ] ) -> integer() | none.
-find_mismatch( [ { _, Y }, { _, Y } = A, { _, Y } = B | T ] ) -> find_mismatch( [ A, B | T ] );
-find_mismatch( [ { E, X }, { _, Y },     { _, Y }     | _ ] ) -> E - ( X - Y );
-find_mismatch( [ { _, Y }, { E, X },     { _, Y }     | _ ] ) -> E - ( X - Y );
-find_mismatch( [ { _, Y }, { _, Y },     { E, X }     | _ ] ) -> E - ( X - Y );
+find_mismatch( [ { _, Y }, { A, Y }, { B, Y } | T ] ) -> find_mismatch( [ { A, Y }, { B, Y } | T ] );
+find_mismatch( [ { E, X }, { _, Y }, { _, Y } | _ ] ) -> E + Y - X;
+find_mismatch( [ { _, Y }, { E, X }, { _, Y } | _ ] ) -> E + Y - X;
+find_mismatch( [ { _, Y }, { _, Y }, { E, X } | _ ] ) -> E + Y - X;
 find_mismatch( _ ) -> none.
 
 -spec get_children( tower_name(), parent_to_children_map() ) -> [ tower_name() ].
@@ -71,18 +69,21 @@ get_children( Parent, ParentToChildrenMap ) ->
 get_weight( Tower, Weights ) ->
     maps:get( Tower, Weights ).
 
--spec find_weight_mismatch( tower_name(), parent_to_children_map(), tower_to_weight_map() ) -> any().
-find_weight_mismatch( Parent, ParentToChildrenMap, Weights ) ->
-    ParentWeight = get_weight( Parent, Weights ),
-    case get_children( Parent, ParentToChildrenMap ) of
-        [] -> { ParentWeight, 0 };
+-spec get_children_load_weights( [ tower_name() ], parent_to_children_map(), tower_to_weight_map() ) -> [ { tower_weight(), tower_weight() } ].
+get_children_load_weights( Children, ParentToChildrenMap, Weights ) ->
+    lists:map( fun( Child ) -> calc_tower_load_weight( Child, ParentToChildrenMap, Weights ) end, Children ).
+
+
+-spec calc_tower_load_weight( tower_name(), parent_to_children_map(), tower_to_weight_map() ) -> { tower_weight(), tower_weight() }.
+calc_tower_load_weight( Tower, ParentToChildrenMap, Weights ) ->
+    TowerWeight = get_weight( Tower, Weights ),
+    case get_children( Tower, ParentToChildrenMap ) of
+        [] -> { TowerWeight, 0 };
         Children ->
-            ChildrenWeights = lists:map( fun( Child ) ->
-                                                 { ChildWeight, SubWeight } = find_weight_mismatch( Child, ParentToChildrenMap, Weights ),
-                                                 { ChildWeight, ChildWeight + SubWeight } end,
-                                         Children ),
-            case find_mismatch( ChildrenWeights ) of
-                none -> { ParentWeight, lists:sum( [ TotalWeight  || { _, TotalWeight } <- ChildrenWeights ] ) };
+            LoadWeights = get_children_load_weights( Children, ParentToChildrenMap, Weights ),
+            TotalWeights = lists:map( fun ( { ChildWeight, LoadWeight } ) -> { ChildWeight, ChildWeight + LoadWeight } end, LoadWeights ),
+            case find_mismatch( TotalWeights ) of
+                none -> { TowerWeight, lists:sum( [ TotalWeight  || { _, TotalWeight } <- TotalWeights ] ) };
                 Correct -> throw( Correct )
             end
     end.
@@ -90,5 +91,5 @@ find_weight_mismatch( Parent, ParentToChildrenMap, Weights ) ->
 -spec solve2( nonempty_string() ) -> integer().
 solve2( Input ) ->
     { ParentToChildrenMap, ChildToParentMap, Weights } = make_tower_maps ( Input ),
-    Top = find_top_parent( ParentToChildrenMap, ChildToParentMap ),
-    catch find_weight_mismatch( Top, ParentToChildrenMap, Weights ).
+    Base = find_base_tower( ParentToChildrenMap, ChildToParentMap ),
+    catch calc_tower_load_weight( Base, ParentToChildrenMap, Weights ).
