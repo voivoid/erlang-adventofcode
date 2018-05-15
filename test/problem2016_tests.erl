f09d970b0b582b1abee0fef32ed5b74da86daae8
Sergey Ivanov <voivoid@mail.ru>
2018-05-15 18:38:23 +0300

    add forgotten tests

--- test/problem2016_tests.erl
+++ test/problem2016_tests.erl
@@ -15,6 +15,8 @@ problem_03_2_test_() -> utils:make_test( "2016_03", fun problem2016_03:solve2/1,
 problem_04_1_test_() -> utils:make_test( "2016_04", fun problem2016_04:solve1/1, 278221 ).
 problem_04_2_test_() -> utils:make_test( "2016_04", fun problem2016_04:solve2/1, 267 ).
 
+problem_05_1_test_() -> utils:make_test( "2016_05", fun problem2016_05:solve1/1, "4543C154" ).
+
 problem_06_1_test_() -> utils:make_test( "2016_06", fun problem2016_06:solve1/1, "wkbvmikb" ).
 problem_06_2_test_() -> utils:make_test( "2016_06", fun problem2016_06:solve2/1, "evakwaga" ).
 
