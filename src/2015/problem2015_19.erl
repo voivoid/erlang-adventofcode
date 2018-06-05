c056cf532b31cbf2c34fcb11331ab66213ca20a8
Sergey Ivanov <voivoid@mail.ru>
2018-06-05 23:10:45 +0300

    problem2015_19 part 1 partly solved

--- src/2015/problem2015_19.erl
+++ src/2015/problem2015_19.erl
@@ -4,7 +4,7 @@
 
 -type molecula() :: string().
 -type replacement() :: { molecula(), molecula() }.
--type replacements() :: #{ molecula() := molecula() }.
+-type replacements_map() :: #{ molecula() := molecula() }.
 
 %%% COMMON
 
@@ -17,14 +17,14 @@ parse_replacement( Replacement ) ->
     [ From, To ] = string:tokens( Replacement, " =>" ),
     { From, To }.
 
--spec parse_replacements( [ string() ] ) -> replacements().
-parse_replacements( Replacements ) ->
+-spec parse_ReplacementsMap( [ string() ] ) -> replacements_map().
+parse_ReplacementsMap( ReplacementsMap ) ->
     lists:foldl( fun( Replacement, AccMap ) ->
                          { From, To } = parse_replacement( Replacement ),
                          maps:update_with( From, fun( Value ) -> [ To | Value ]  end, [ To ], AccMap )
                  end,
                  #{},
-                 Replacements ).
+                 ReplacementsMap ).
 
 -spec split_molecula( molecula() ) -> [ molecula() ].
 split_molecula( [] ) -> [];
@@ -32,24 +32,35 @@ split_molecula( [ M | MS ] ) ->
     { Left, Right } = lists:splitwith( fun is_lowercase/1, MS ),
     [ [ M | Left ] | split_molecula( Right ) ].
 
--spec parse_input( string() ) -> { [ molecula() ], replacements() }.
+-spec parse_input( string() ) -> { [ molecula() ], replacements_map() }.
 parse_input( Input ) ->
     Lines = string:tokens( Input, "\n" ),
-    { Replacements, Molecula } = lists:split( erlang:length( Lines ) - 1, Lines ),
-    { split_molecula( string:trim( Molecula ) ), parse_replacements( Replacements ) }.
+    { ReplacementsMap, Molecula } = lists:split( erlang:length( Lines ) - 1, Lines ),
+    { split_molecula( string:trim( Molecula ) ), parse_ReplacementsMap( ReplacementsMap ) }.
 
-find_replacements( Molecula, Replacements ) ->
-    maps:get( Molecula, Replacements, Molecula ).
+find_replacement( Molecula, ReplacementsMap ) ->
+    maps:get( Molecula, ReplacementsMap, [ Molecula ] ).
 
-replace_moleculas( Moleculas, Replacements ) ->
-    [].
+molecula_array_to_str( Array ) ->
+      array:foldr( fun( _, Molecula, Acc ) ->
+                           Molecula ++ Acc
+                   end,
+                   "",
+                   Array ).
+
+
+replace_moleculas( Moleculas, ReplacementsMap ) ->
+    MoleculasArray = array:from_list( Moleculas ),
+    IndexedMoleculas = lists:zip( Moleculas, lists:seq( 0, array:size( MoleculasArray ) - 1 ) ),
+    [ molecula_array_to_str( array:set( I, ReplacedMolecula, MoleculasArray ) ) || { Molecula, I } <- IndexedMoleculas, ReplacedMolecula <- find_replacement( Molecula, ReplacementsMap ) ].
 
 %%% PART 1
 
 -spec solve1( string() ) -> non_neg_integer().
 solve1( Input ) ->
-    { Moleculas, Replacements } = parse_input( Input ),
-    replace_moleculas( Moleculas, Replacements ).
+    { Moleculas, ReplacementsMap } = parse_input( Input ),
+    Replacements = replace_moleculas( Moleculas, ReplacementsMap ),
+    erlang:length( lists:usort( Replacements ) ).
 
 %%% PART 2
 
--- test/problem2015_tests.erl
+++ test/problem2015_tests.erl
@@ -1,74 +1,74 @@
 -module( problem2015_tests ).
 -include_lib("eunit/include/eunit.hrl").
 
-problem_01_1() -> utils:make_test( "2015_01", fun problem2015_01:solve1/1, 280 ).
-problem_01_2() -> utils:make_test( "2015_01", fun problem2015_01:solve2/1, 1797 ).
+%% problem_01_1() -> utils:make_test( "2015_01", fun problem2015_01:solve1/1, 280 ).
+%% problem_01_2() -> utils:make_test( "2015_01", fun problem2015_01:solve2/1, 1797 ).
 
-problem_02_1() -> utils:make_test( "2015_02", fun problem2015_02:solve1/1, 1586300 ).
-problem_02_2() -> utils:make_test( "2015_02", fun problem2015_02:solve2/1, 3737498 ).
+%% problem_02_1() -> utils:make_test( "2015_02", fun problem2015_02:solve1/1, 1586300 ).
+%% problem_02_2() -> utils:make_test( "2015_02", fun problem2015_02:solve2/1, 3737498 ).
 
-problem_03_1() -> utils:make_test( "2015_03", fun problem2015_03:solve1/1, 2592 ).
-problem_03_2() -> utils:make_test( "2015_03", fun problem2015_03:solve2/1, 2360 ).
+%% problem_03_1() -> utils:make_test( "2015_03", fun problem2015_03:solve1/1, 2592 ).
+%% problem_03_2() -> utils:make_test( "2015_03", fun problem2015_03:solve2/1, 2360 ).
 
-problem_04_1() -> utils:make_test( "2015_04", fun problem2015_04:solve1/1, 254575 ).
-problem_04_2() -> utils:make_test( "2015_04", fun problem2015_04:solve2/1, 1038736 ).
+%% problem_04_1() -> utils:make_test( "2015_04", fun problem2015_04:solve1/1, 254575 ).
+%% problem_04_2() -> utils:make_test( "2015_04", fun problem2015_04:solve2/1, 1038736 ).
 
-problem_05_1() -> utils:make_test( "2015_05", fun problem2015_05:solve1/1, 255 ).
-problem_05_2() -> utils:make_test( "2015_05", fun problem2015_05:solve2/1, 55 ).
+%% problem_05_1() -> utils:make_test( "2015_05", fun problem2015_05:solve1/1, 255 ).
+%% problem_05_2() -> utils:make_test( "2015_05", fun problem2015_05:solve2/1, 55 ).
 
-problem_07_1() -> utils:make_test( "2015_07", fun problem2015_07:solve1/1, 46065 ).
-problem_07_2() -> utils:make_test( "2015_07", fun problem2015_07:solve2/1, 14134 ).
+%% problem_07_1() -> utils:make_test( "2015_07", fun problem2015_07:solve1/1, 46065 ).
+%% problem_07_2() -> utils:make_test( "2015_07", fun problem2015_07:solve2/1, 14134 ).
 
-problem_08_1() -> utils:make_test( "2015_08", fun problem2015_08:solve1/1, 1333 ).
-problem_08_2() -> utils:make_test( "2015_08", fun problem2015_08:solve2/1, 2046 ).
+%% problem_08_1() -> utils:make_test( "2015_08", fun problem2015_08:solve1/1, 1333 ).
+%% problem_08_2() -> utils:make_test( "2015_08", fun problem2015_08:solve2/1, 2046 ).
 
-problem_09_1() -> utils:make_test( "2015_09", fun problem2015_09:solve1/1, 207 ).
-problem_09_2() -> utils:make_test( "2015_09", fun problem2015_09:solve2/1, 804 ).
+%% problem_09_1() -> utils:make_test( "2015_09", fun problem2015_09:solve1/1, 207 ).
+%% problem_09_2() -> utils:make_test( "2015_09", fun problem2015_09:solve2/1, 804 ).
 
-problem_10_1() -> utils:make_test( "2015_10", fun problem2015_10:solve1/1, 252594 ).
-problem_10_2() -> utils:make_test( "2015_10", fun problem2015_10:solve2/1, 3579328 ).
+%% problem_10_1() -> utils:make_test( "2015_10", fun problem2015_10:solve1/1, 252594 ).
+%% problem_10_2() -> utils:make_test( "2015_10", fun problem2015_10:solve2/1, 3579328 ).
 
-problem_11_1() -> utils:make_test( "2015_11", fun problem2015_11:solve1/1, "hxbxxyzz" ).
-problem_11_2() -> utils:make_test( "2015_11", fun problem2015_11:solve2/1, "hxcaabcc" ).
+%% problem_11_1() -> utils:make_test( "2015_11", fun problem2015_11:solve1/1, "hxbxxyzz" ).
+%% problem_11_2() -> utils:make_test( "2015_11", fun problem2015_11:solve2/1, "hxcaabcc" ).
 
-problem_12_1() -> utils:make_test( "2015_12", fun problem2015_12:solve1/1, 119433 ).
-problem_12_2() -> utils:make_test( "2015_12", fun problem2015_12:solve2/1, 68466 ).
+%% problem_12_1() -> utils:make_test( "2015_12", fun problem2015_12:solve1/1, 119433 ).
+%% problem_12_2() -> utils:make_test( "2015_12", fun problem2015_12:solve2/1, 68466 ).
 
-problem_13_1() -> utils:make_test( "2015_13", fun problem2015_13:solve1/1, 618 ).
-problem_13_2() -> utils:make_test( "2015_13", fun problem2015_13:solve2/1, 601 ).
+%% problem_13_1() -> utils:make_test( "2015_13", fun problem2015_13:solve1/1, 618 ).
+%% problem_13_2() -> utils:make_test( "2015_13", fun problem2015_13:solve2/1, 601 ).
 
-problem_14_1() -> utils:make_test( "2015_14", fun problem2015_14:solve1/1, 2660 ).
-problem_14_2() -> utils:make_test( "2015_14", fun problem2015_14:solve2/1, 1256 ).
+%% problem_14_1() -> utils:make_test( "2015_14", fun problem2015_14:solve1/1, 2660 ).
+%% problem_14_2() -> utils:make_test( "2015_14", fun problem2015_14:solve2/1, 1256 ).
 
-problem_15_1() -> utils:make_test( "2015_15", fun problem2015_15:solve1/1, 13882464 ).
-problem_15_2() -> utils:make_test( "2015_15", fun problem2015_15:solve2/1, 11171160 ).
+%% problem_15_1() -> utils:make_test( "2015_15", fun problem2015_15:solve1/1, 13882464 ).
+%% problem_15_2() -> utils:make_test( "2015_15", fun problem2015_15:solve2/1, 11171160 ).
 
-problem_16_1() -> utils:make_test( "2015_16", fun problem2015_16:solve1/1, 103 ).
-problem_16_2() -> utils:make_test( "2015_16", fun problem2015_16:solve2/1, 405 ).
+%% problem_16_1() -> utils:make_test( "2015_16", fun problem2015_16:solve1/1, 103 ).
+%% problem_16_2() -> utils:make_test( "2015_16", fun problem2015_16:solve2/1, 405 ).
 
-problem_17_1() -> utils:make_test( "2015_17", fun problem2015_17:solve1/1, 4372 ).
-problem_17_2() -> utils:make_test( "2015_17", fun problem2015_17:solve2/1, 4 ).
+%% problem_17_1() -> utils:make_test( "2015_17", fun problem2015_17:solve1/1, 4372 ).
+%% problem_17_2() -> utils:make_test( "2015_17", fun problem2015_17:solve2/1, 4 ).
 
-problem_18_1() -> utils:make_test( "2015_18", fun problem2015_18:solve1/1, 0 ).
+problem_19_1() -> utils:make_test( "2015_19", fun problem2015_19:solve1/1, 0 ).
 
 
 problem2015_test_() ->
     { inparallel, [
-                   problem_01_1(), problem_01_2(),
-                   problem_02_1(), problem_02_2(),
-                   problem_03_1(), problem_03_2(),
-                   problem_04_1(), problem_04_2(),
-                   problem_05_1(), problem_05_2(),
-                   problem_07_1(), problem_07_2(),
-                   problem_08_1(), problem_08_2(),
-                   problem_09_1(), problem_09_2(),
-                   problem_10_1(), problem_10_2(),
-                   problem_11_1(), problem_11_2(),
-                   problem_12_1(), problem_12_2(),
-                   problem_13_1(), problem_13_2(),
-                   problem_14_1(), problem_14_2(),
-                   problem_15_1(), problem_15_2(),
-                   problem_16_1(), problem_16_2(),
-                   problem_17_1(), problem_17_2(),
-                   problem_18_1()
+                   %% problem_01_1(), problem_01_2(),
+                   %% problem_02_1(), problem_02_2(),
+                   %% problem_03_1(), problem_03_2(),
+                   %% problem_04_1(), problem_04_2(),
+                   %% problem_05_1(), problem_05_2(),
+                   %% problem_07_1(), problem_07_2(),
+                   %% problem_08_1(), problem_08_2(),
+                   %% problem_09_1(), problem_09_2(),
+                   %% problem_10_1(), problem_10_2(),
+                   %% problem_11_1(), problem_11_2(),
+                   %% problem_12_1(), problem_12_2(),
+                   %% problem_13_1(), problem_13_2(),
+                   %% problem_14_1(), problem_14_2(),
+                   %% problem_15_1(), problem_15_2(),
+                   %% problem_16_1(), problem_16_2(),
+                   %% problem_17_1(), problem_17_2(),
+                   problem_19_1()
                   ] }.
